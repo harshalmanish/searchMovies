f781414742dff4e420aa5a3f9d970a260cde9d7d
harshalmanish <harshalmanish@gmail.com>
2020-01-13 13:12:06 +0530

    Updates

---
+++ precache-manifest.b2120fd7c0ef6ac6d8760aaa433dd44a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6488e98c5434e7ea6bbb521f3dd64a25",
+    "url": "/searchMovies/index.html"
+  },
+  {
+    "revision": "9f4243f84b612c8ed922",
+    "url": "/searchMovies/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "29c1132ac368e3b66a6b",
+    "url": "/searchMovies/static/css/main.f3bc9cb4.chunk.css"
+  },
+  {
+    "revision": "9f4243f84b612c8ed922",
+    "url": "/searchMovies/static/js/2.7bc05f09.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/searchMovies/static/js/2.7bc05f09.chunk.js.LICENSE"
+  },
+  {
+    "revision": "29c1132ac368e3b66a6b",
+    "url": "/searchMovies/static/js/main.00f9cbe3.chunk.js"
+  },
+  {
+    "revision": "4c36682dad3a4c630b28",
+    "url": "/searchMovies/static/js/runtime-main.6dce305a.js"
+  },
+  {
+    "revision": "e7d77243134380616a9f3d2ecedf5da6",
+    "url": "/searchMovies/static/media/image-unavailable-icon-260nw-1157415967.e7d77243.webp"
+  },
+  {
+    "revision": "2f9bba45933143e3b4135df475041dc3",
+    "url": "/searchMovies/static/media/unavailable-image.2f9bba45.jpg"
+  }
+]);
